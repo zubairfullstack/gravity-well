40bed7b39ba609082af1d82710643cda80f84c78
Zubair Anwar <zubairanwar@icloud.com>
2019-04-21 21:25:51 -0500

    (1) client index.js part of boilermaker

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <div>Canvas</div>,
+  document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+);
